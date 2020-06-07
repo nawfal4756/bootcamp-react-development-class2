fb257c3cd6b85d55ea01e71493a2fcf15b7ec619
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-06-07 14:55:16 +0500

    new react app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+function Hi() {
+  return <div>
+            <div className="heading">Hello <strong>Nawfal</strong>!</div>
+            <ul>
+              <li>I love to code</li>
+              <li>Eat, sleep, code, repeat</li> 
+              <li>My age is: {10+9}</li>
+            </ul>
+          </div>
+}
+
+ReactDOM.render(<Hi/>,document.querySelector('#root'))
