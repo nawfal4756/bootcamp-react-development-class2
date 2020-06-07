da2d8f0b0ea1bdfda0b079cd0b919a14ee4f7899
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-06-07 15:07:28 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function Hi() {
             <div className="heading">Hello <strong>Nawfal</strong>!</div>
             <ul>
               <li>I love to code</li>
-              <li>Eat, sleep, code, repeat</li> 
+              <li>Eat, sleep, code, repeat!</li> 
               <li>My age is: {10+9}</li>
             </ul>
           </div>
